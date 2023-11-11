d387ec536fa646ed3b6a4cd65aa5a59ba67460ce
im_pig_ggul_ggul <141539089+pigggulggul@users.noreply.github.com>
2023-11-11 19:13:50 +0900

    [Bronze V] Title: Hello World, Time: 72 ms, Memory: 11360 KB -BaekjoonHub

---
+++ 백준/Bronze/2557. Hello World/Hello World.java
@@ -0,0 +1,11 @@
+import java.io.*;
+import java.util.*;
+public class Main {
+	static int N, M, T;
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		System.out.println("Hello World!");
+	}
+
+}
