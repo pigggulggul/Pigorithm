b46b3b223f486f9b671617d0d5c1fc266473f435
im_pig_ggul_ggul <141539089+pigggulggul@users.noreply.github.com>
2024-02-25 01:31:22 +0900

    [Gold IV] Title: 녹색 옷 입은 애가 젤다지?, Time: 180 ms, Memory: 17504 KB -BaekjoonHub

--- 백준/Gold/4485. 녹색 옷 입은 애가 젤다지？/녹색 옷 입은 애가 젤다지？.java
+++ 백준/Gold/4485. 녹색 옷 입은 애가 젤다지？/녹색 옷 입은 애가 젤다지？.java
@@ -1,91 +1,80 @@
 import java.util.*;
 import java.io.*;
 
-class Node implements Comparable<Node> {
+
+class Maze implements Comparable<Maze>{
+
 	int x;
 	int y;
 	int cost;
-
-	public Node(int x, int y, int cost) {
-		this.x = x;
-		this.y = y;
-		this.cost = cost;
+	
+	public Maze(int x,int y, int cost) {
+		this.x=x;
+		this.y=y;
+		this.cost=cost;
 	}
-
+	
 	@Override
-	public int compareTo(Node o) {
-		return this.cost - o.cost;
+	public int compareTo(Maze o) {
+		// TODO Auto-generated method stub
+		return this.cost-o.cost;
 	}
+	
 }
 
 public class Main {
-	static int N = 0;
-	static int maze[][];
-	static int min;
-	static int dx[] = { -1, 0, 1, 0 };
-	static int dy[] = { 0, 1, 0, -1 };
-	static int testcase = 0;
+	static int N, M;
+	static int arr[][];
 	static int dist[][];
-
+	static int dx[] = {0,1,0,-1};
+	static int dy[] = {-1,0,1,0};
+	static int count=1;
+	static PriorityQueue<Maze> pq = new PriorityQueue<>();
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st;
-		StringBuilder sb = new StringBuilder();
+		
+		
 		N = Integer.parseInt(br.readLine());
-		// 한 번 이동 시 마다 해당 소지금 만큼 돈을 잃는다.돈을 최소화하여 배열 끝까지 도착
-		// 더이상 갈 수 없거나 N-1,N-1에 도착하면 종료
-		while (N != 0) {
-			// 초기화
-			maze = new int[N][N];
+		while(N != 0) {
+			arr=new int[N][N];
 			dist = new int[N][N];
-			min = Integer.MAX_VALUE;
-			testcase++;
+			
+			//초기화
 			for (int i = 0; i < N; i++) {
-				st = new StringTokenizer(br.readLine());
+				st= new StringTokenizer(br.readLine());
 				for (int j = 0; j < N; j++) {
-					maze[i][j] = Integer.parseInt(st.nextToken());
+					arr[i][j] = Integer.parseInt(st.nextToken());
 					dist[i][j] = Integer.MAX_VALUE;
 				}
 			}
-			dist[0][0] = maze[0][0];
-			PriorityQueue<Node> pq = new PriorityQueue<>();
-			pq.add(new Node(0, 0, maze[0][0]));
-			while (!pq.isEmpty()) {
-				Node node = pq.poll();
-
+			dist[0][0]=arr[0][0];
+			pq.add(new Maze(0,0,dist[0][0]));
+			
+			while(!pq.isEmpty()) {
+				Maze info = pq.poll();
 				for (int d = 0; d < 4; d++) {
-					int nx = node.x + dx[d];
-					int ny = node.y + dy[d];
+					int nx = info.x+dx[d];
+					int ny = info.y+dy[d];
 					
-					if(!check(nx,ny)) continue;
-					if(dist[nx][ny]>dist[node.x][node.y]+maze[nx][ny]) {
-						dist[nx][ny] = dist[node.x][node.y]+maze[nx][ny];
-						pq.add(new Node(nx,ny,dist[nx][ny]));
+					if(!checkWall(nx,ny)) {
+						continue;
+					}
+					
+					if(dist[nx][ny] > dist[info.x][info.y]+arr[nx][ny]) {
+						dist[nx][ny] = dist[info.x][info.y]+arr[nx][ny];
+						pq.offer(new Maze(nx,ny,dist[nx][ny]));
 					}
 				}
-
 			}
-
-			sb.append("Problem " + testcase + ": " + dist[N-1][N-1]).append("\n");
-//			System.out.println("Problem " + testcase + ": " + min);
-			N = Integer.parseInt(br.readLine());
-
+			System.out.println("Problem "+count+": "+ dist[N-1][N-1]);	
+			N=Integer.parseInt(br.readLine());
+			count++;
 		}
-		System.out.println(sb.toString());
+		
 	}
 
-	private static void printboolean(int[][] visited) {
-		for (int i = 0; i < N; i++) {
-			for (int j = 0; j < N; j++) {
-				System.out.print(visited[i][j] + " ");
-			}
-			System.out.println();
-		}
-		System.out.println();
+	private static boolean checkWall(int x, int y) {
+		return x>=0 && x<N &&  y>=0 && y<N;
 	}
-
-	private static boolean check(int x, int y) {
-		return x >= 0 && x < N && y >= 0 && y < N;
-	}
-
 }
