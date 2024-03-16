1f24f003fe6960b109c8d69df1a4b2e167f2bbbf
im_pig_ggul_ggul <141539089+pigggulggul@users.noreply.github.com>
2024-03-16 16:31:51 +0900

    [Gold IV] Title: 네트워크 연결, Time: 456 ms, Memory: 45712 KB -BaekjoonHub

--- 백준/Gold/1922. 네트워크 연결/네트워크 연결.java
+++ 백준/Gold/1922. 네트워크 연결/네트워크 연결.java
@@ -1,90 +1,73 @@
 import java.util.*;
 import java.io.*;
+class Kruskal_graph implements Comparable<Kruskal_graph>{
+    int v;
+    int w;
+    int cost;
 
+    public Kruskal_graph(int v,int w,int cost){
+        this.v=v;
+        this.w=w;
+        this.cost=cost;
+    }
 
-class Edge implements Comparable<Edge> {
-	int w;
-	int e;
-	int cost;
-	
-	public Edge(int w, int e, int cost) {
-		this.w = w;
-		this.e = e;
-		this.cost = cost;
-	}
-	
-	@Override
-	public int compareTo(Edge edge) {
-		return this.cost - edge.cost;
-	}
-
-	@Override
-	public String toString() {
-		return "Edge [w=" + w + ", e=" + e + ", cost=" + cost + "]";
-	}
-	
-	
+    @Override
+    public int compareTo(Kruskal_graph node){
+        return this.cost - node.cost;
+    }
 }
 
 public class Main {
-	static int N, M, T;
-	static PriorityQueue<Edge> pq = new PriorityQueue<>();
-	static int parents[];
-	public static void main(String[] args) throws Exception {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st;
-		N= Integer.parseInt(br.readLine());
-		M= Integer.parseInt(br.readLine());
-		parents= new int[N+1];
-		for (int i = 0; i < M; i++) {
-			st = new StringTokenizer(br.readLine());
-			int a = Integer.parseInt(st.nextToken());
-			int b = Integer.parseInt(st.nextToken());
-			int c = Integer.parseInt(st.nextToken());
-			pq.offer(new Edge(a,b,c));
-		}
-		makeSet();
-		//		printArr();
-		int result = 0;
-		
-		while(!pq.isEmpty()) {
-			Edge info = pq.poll();
-			
-			if(find(info.w) == find(info.e)) continue;
-			union(info.w,info.e);
-			result+=info.cost;
-			
-		}
-		System.out.println(result);
-	}
+    static int N,M;
+    static PriorityQueue<Kruskal_graph> queue = new PriorityQueue();
+    static int[] parents;
+    static int result=0;
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        N= Integer.parseInt(br.readLine());
+        M= Integer.parseInt(br.readLine());
+    parents = new int[N+1];
+        for (int i = 0 ; i < M ; i++){
+            st= new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            int c = Integer.parseInt(st.nextToken());
+            queue.offer(new Kruskal_graph(a,b,c));
+        }
 
-	private static void makeSet() {
-		for (int i = 0; i < N; i++) {
-			parents[i]=i;
-		}
-	}
-	private static int find(int a) {
-		if(parents[a]==a) {
-			return a;
-		}
-		return parents[a]=find(parents[a]);
-	}
-	private static void union(int a,int b) {
-		int x = find(a);
-		int y = find(b);
-		if(x > y) {
-			parents[y]= x;
-		}else {
-			parents[x]= y;
-		}
-	}	
+        makeSet();
 
-	private static void printArr() {
-		for (int i = 0; i < N; i++) {
-			for (int j = 0; j < N; j++) {
-				System.out.print("");
-			}
-			System.out.println();
-		}
-	}
+        while(!queue.isEmpty()){
+            Kruskal_graph now_graph = queue.poll();
+            if(find(now_graph.v) == find(now_graph.w)) continue;
+            else{
+                union(now_graph.v,now_graph.w);
+                result += now_graph.cost;
+            }
+        }
+        System.out.println(result);
+    }
+
+    private static void union(int x, int y) {
+        int a = find(x);
+        int b = find(y);
+
+        if( a> b){
+            parents[a] = b;
+        }else{
+            parents[b]=a;
+        }
+    }
+
+    private static void makeSet() {
+        for (int i = 0 ; i <=N ; i++){
+            parents[i]=i;
+        }
+    }
+    private static int find(int x){
+        if(parents[x]==x){
+            return x;
+        }else return parents[x] = find(parents[x]);
+    }
 }
