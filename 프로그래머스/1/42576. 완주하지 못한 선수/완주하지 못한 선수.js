8042d6cb25f2ab51e13f7b441a2a558a7698c747
im_pig_ggul_ggul <141539089+pigggulggul@users.noreply.github.com>
2024-06-03 15:57:20 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 125.66 ms, Memory: 58.3 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.js
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.js
@@ -1,18 +1,17 @@
 function solution(participant, completion) {
     let answer="";
     participant.sort((a,b)=>{
-        if(a > b){
-            return 1
-        }else {
-            return -1
-        }
+        if(a>b) return 1;
+        else return -1;
     });
-    completion.sort((a,b)=>{if(a>b){return 1} else {return -1}})
-    for(let i = 0 ; i < participant.length ; i++){
-        if(participant[i] !== completion[i]){
-            answer=participant[i];
-            break;
+    completion.sort((a,b)=>{
+        if(a>b) return 1;
+        else return -1;
+    });
+    for(var i = 0 ; i < completion.length ; i++){
+        if(completion[i] !== participant[i]){
+            return participant[i];
         }
     }
-    return answer;
+    return participant[participant.length-1];
 }
