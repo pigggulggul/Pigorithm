95c460bec01d23ecdde61cffc263e487a8a2f2e8
im_pig_ggul_ggul <141539089+pigggulggul@users.noreply.github.com>
2024-07-08 12:33:46 +0900

    [Silver I] Title: 회의실 배정, Time: 6256 ms, Memory: 45636 KB -BaekjoonHub

---
+++ 백준/Silver/1931. 회의실 배정/회의실 배정.js
@@ -0,0 +1,31 @@
+const input = require("fs")
+  .readFileSync("/dev/stdin")
+  .toString()
+  .trim()
+  .split("\n");
+
+let N = Number(input.shift());
+// 각 회의 i에 대해 시작시간과 끝나는 시간이 주어져 있고
+// 회의의 최대 개수 찾기
+// 끝나는 시간이 필요
+let arr = [];
+for (let i = 0; i < N; i++) {
+  let [item1, item2] = input.shift().split(" ").map(Number);
+  arr.push([item1, item2]);
+}
+arr.sort((a, b) => {
+  if (a[1] === b[1]) {
+    return a[0] - b[0];
+  }
+  return a[1] - b[1];
+});
+
+let time = 0;
+let count = 0;
+for (let i = 0; i < N; i++) {
+  if (time <= arr[i][0]) {
+    time = arr[i][1];
+    count++;
+  }
+}
+console.log(count);
