d6cec63fa6c8811b3cedf8dbe9cf031a4b52ac17
im_pig_ggul_ggul <141539089+pigggulggul@users.noreply.github.com>
2024-08-05 12:25:11 +0900

    [Silver IV] Title: 스위치와 램프, Time: 240 ms, Memory: 12052 KB -BaekjoonHub

--- 백준/Silver/16960. 스위치와 램프/스위치와 램프.js
+++ 백준/Silver/16960. 스위치와 램프/스위치와 램프.js
@@ -1,21 +1,3 @@
-//N개의 스위치 M개의 램프
-// 1번~ N번까지의 연결된 램프의 수
-
-// arr만들기 arr마다 램프에 대한 정보가 있다
-// ex) arr[0] = [1,3,5]
-// arr[1] = [1,2]
-// arr[2] = [3,4,5]
-// arr[3] = [1,1]
-
-// 램프 flag 를 lamp 개수만큼 만들기
-
-// sortNum=0부터 시작
-// i를 arr[0]~arr[i]번까지 돌면서 arr[i] 안에 있는 숫자에 해당하는 lamp[]의 값을 false면 true로 바꾸기
-// i === sortNum이면 continue;
-// 바꿀 때 falgCount++;
-// 다 돌았을때 flagCount === lamp result=1;
-// 아니면 continue
-
 const [NM, ...input] = require("fs")
   .readFileSync("/dev/stdin")
   .toString()
@@ -36,7 +18,6 @@ let result = 0;
 let flagCount;
 let sortNum;
 let lampFlag;
-// console.log(newArr);
 for (let start = 0; start < N; start++) {
   sortNum = start;
   lampFlag = new Array(M + 1).fill(false);
@@ -52,7 +33,6 @@ for (let start = 0; start < N; start++) {
       }
     }
   }
-  // console.log(lampFlag);
   if (flagCount === M) {
     result = 1;
     break;
