b15a277fe82bb3cff673347de085f45befc27d3c
im_pig_ggul_ggul <141539089+pigggulggul@users.noreply.github.com>
2024-09-13 17:42:35 +0900

    [Gold V] Title: 경쟁적 전염, Time: 240 ms, Memory: 19508 KB -BaekjoonHub

--- 백준/Gold/18405. 경쟁적 전염/경쟁적 전염.java
+++ 백준/Gold/18405. 경쟁적 전염/경쟁적 전염.java
@@ -12,7 +12,6 @@ class InfectionPos {
 		this.num = num;
 	}
 }
-
 public class Main {
 
 	static int N, K, S;
@@ -26,10 +25,6 @@ public class Main {
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
-		// NxN칸
-		// 낮은 번호부터 증식
-		// S초 후에 x,y에 존재하는 바이러스 출력. 없으면 0
-		// 왼쪽위 11부터 시작
 		N = Integer.parseInt(st.nextToken());
 		K = Integer.parseInt(st.nextToken());
 		arr = new int[N][N];
@@ -54,17 +49,7 @@ public class Main {
 		}
 
 		System.out.println(arr[resultX][resultY]);
-		// N K 배열과 최대숫자
-		// N개의ㅡ 배열
-		// S X Y S초후에 x,y위치의 값.
-
-		// 1 1 2
-		// 0 0 2
-		// 3 3 0
-		// 배열 복사하고 돌면서 1번부터 찾으면 3방향해서 0이면
 	}
-
-	// 큐에 넣자
 	private static void startInfection(int num) {
 		for (int i = 0; i < N; i++) {
 			for (int j = 0; j < N; j++) {
@@ -75,7 +60,6 @@ public class Main {
 			}
 		}
 	}
-
 	private static void goInfection(int[][] arr) {
 		int size = queue.size();
 
@@ -98,11 +82,9 @@ public class Main {
 			}
 		}
 	}
-
 	private static boolean checkWall(int x, int y) {
 		return x >= 0 && x < N && y >= 0 && y < N;
 	}
-
 	private static void printArr(int[][] arr) {
 		for (int i = 0; i < N; i++) {
 			for (int j = 0; j < N; j++) {
